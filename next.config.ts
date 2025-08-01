fda16cc10a58216c5b42bb1bda9321a9ab7f5cf9
habuiquang2000 <habuiquang2000@gmail.com>
2025-08-01 15:18:24 +0700

    config env

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,30 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.html$/i,
+      loader: "html-loader",
+    });
+    config.module.rules.push({
+      test: /\.liquid$/,
+      issuer: { and: [/\.(js|ts)x?$/] },
+      use: [
+        {
+          loader: 'html-loader',
+          options: {
+            minimize: false,
+            // TUYỆT ĐỐI TẮT xử lý src/href… để bundler không "require" hình
+            sources: false,
+            // Đảm bảo xuất CommonJS, tránh esModule import quirks
+            esModule: false,
+          }
+        }
+      ]
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
